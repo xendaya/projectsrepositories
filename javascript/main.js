8a025f1227bf33b3579df8d4a726c7421be3484d
ketera21 <77958432+ketera21@users.noreply.github.com>
2021-07-20 14:58:32 +0500

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+
+const e = document.querySelector('#annotate-one');
+const annotation = RoughNotation.annotate(e, { type: 'box', color: 'black', padding: 7, strokeWidth: 6});
+annotation.show();
+
---
+++ javascript/notation.js
@@ -0,0 +1,11 @@
+
+
+const e = document.querySelector('#bracket-span');
+const annotation = RoughNotation.annotate(e, { type: 'box', color: 'red', padding: 7, strokeWidth: 3});
+annotation.show();
+
+
+
+
+
+
