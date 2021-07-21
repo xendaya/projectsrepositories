d8e61259177d324d56d32f8a5c6421bdfd619259
ketera21 <77958432+ketera21@users.noreply.github.com>
2021-07-21 11:23:13 +0500

    Update notation.js

--- javascript/notation.js
+++ javascript/notation.js
@@ -1,8 +1,11 @@
 
 //Usage for  only one text
+
 const e = document.querySelector('#bracket-span');
 const annotation = RoughNotation.annotate(e, { type: 'box', color: 'red', padding: 7, strokeWidth: 3});
 annotation.show();
+
+
 
 
 
