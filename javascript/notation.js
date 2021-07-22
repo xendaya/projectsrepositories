d7523a6a2835048e3ea6f17f07e09342ebd7afe7
ketera21 <77958432+ketera21@users.noreply.github.com>
2021-07-22 22:28:44 +0500

    Update notation.js

--- javascript/notation.js
+++ javascript/notation.js
@@ -11,6 +11,13 @@ const e = document.querySelector('#bracket-span');
 const annotate = RoughNotation.annotate(e1, {type: 'highlight', color: 'black', padding: 7, strokeWidth: 3});
 annotate.show();
 
+// Same goes to everything and you choose where to implement it
+
+const e1 = document.querySelector('#bracket-span');
+const annotate = RoughNotation.annotatee(e1, {type: 'highlight', color: 'black', padding: 7, strokeWidth: 3});
+annotatee.show();
+
+
 
 
 
