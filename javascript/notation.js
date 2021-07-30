5e6f89b2bc12648ea3a647c1d762494cb4c57088
khetera <77958432+khetera@users.noreply.github.com>
2021-07-30 23:13:22 +0500

    Update notation.js

--- javascript/notation.js
+++ javascript/notation.js
@@ -13,7 +13,11 @@ annotate.show();
 
 // Same goes to everything and you choose where to implement it
 
-//Make a copy of the code and just change the element
+//Make a copy of the code and just change the element name
+/*
+for example:
+  change const e1 to = const e2 && 
+*/
 const e1 = document.querySelector('#bracket-span');
 const annotate = RoughNotation.annotatee(e1, {type: 'highlight', color: 'black', padding: 7, strokeWidth: 3});
 annotatee.show();
