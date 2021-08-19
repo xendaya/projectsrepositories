90d787eaab056e5fea1c15daa0488ccbafc24518
ジョーマーロブ <77958432+joemerov@users.noreply.github.com>
2021-08-19 19:44:57 +0500

    notation.js update

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-
+// Rough.js Animation
 const e = document.querySelector('#annotate-one');
 const annotation = RoughNotation.annotate(e, { type: 'box', color: 'black', padding: 7, strokeWidth: 6});
 annotation.show();
--- javascript/notation.js
+++ javascript/notation.js
@@ -22,6 +22,13 @@ const e1 = document.querySelector('#bracket-span');
 const annotate = RoughNotation.annotatee(e1, {type: 'highlight', color: 'black', padding: 7, strokeWidth: 3});
 annotatee.show();
 
+const e2 = document.querySelector('#bracket-header');
+const annotatetwo = RoughNotation.annotatetwo(e2, {type: 'crossthrough', color: 'black', padding: 7});
+annotatetwo.show();
+
+/**
+ * You can edit these codes or add more by changing the identifier to something else(name or number)
+ */
 
 
 
