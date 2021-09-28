b4b966573a34d82a556e1e8687a4ed6312f16559
jackudrew <77958432+jackudrew@users.noreply.github.com>
2021-09-28 16:34:06 +0500

    rough notation style guide

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,8 @@
  * rough notation animation used for highlighting text on hover
  * please search for other styles on the official website in (https://www.roughnotation.com)
  */
+
+// place change the RoughNotation.annotate for various styles 
 const e = document.querySelector('#annotate-one');
 const annotation = RoughNotation.annotate(e, { type: 'box', color: 'black', padding: 7, strokeWidth: 6});
 annotation.show();
