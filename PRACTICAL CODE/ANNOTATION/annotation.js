3db01bd399abe10db8dab376a3006cb248fbd64f
michixosan <77958432+michixosan@users.noreply.github.com>
2021-11-04 20:37:40 +0500

    Update annotation.js
    commented guide

--- PRACTICAL CODE/ANNOTATION/annotation.js
+++ PRACTICAL CODE/ANNOTATION/annotation.js
@@ -1,4 +1,4 @@
 
-const e = document.querySelector('#annotate-one');
-const annotationOne = RoughNotation.annotate(e, { type: 'highlight', color: 'yellow' });
-annotationOne.show();
+const e = document.querySelector('#annotate-one');    // edit the "id" code (#annotate-one) in my case.
+const annotationOne = RoughNotation.annotate(e, { type: 'highlight', color: 'yellow' });  // choose the (padding, type, color and margin(not yet available))
+annotationOne.show(); // .show() to visually see the affect of the library.
