e66eba3b3688bce6377b79586b937457322d81ba
michixosan <77958432+michixosan@users.noreply.github.com>
2021-11-04 20:26:45 +0500

    Update notation.js
    annotation code fix (e1)

--- javascript/notation.js
+++ javascript/notation.js
@@ -4,16 +4,16 @@ const annotation = RoughNotation.annotate(e, { type: 'box', color: 'red', paddin
 annotation.show();
 
 
-const e = document.querySelector('#bracket-span');
+const e1 = document.querySelector('#bracket-span');
 const annotate = RoughNotation.annotate(e1, {type: 'highlight', color: 'black', padding: 7, strokeWidth: 3});
 annotate.show();
 
-const e1 = document.querySelector('#bracket-span');
-const annotate = RoughNotation.annotatee(e1, {type: 'highlight', color: 'black', padding: 7, strokeWidth: 3});
+const e2 = document.querySelector('#bracket-span');
+const annotate = RoughNotation.annotatee(e2, {type: 'highlight', color: 'black', padding: 7, strokeWidth: 3});
 annotatee.show();
 
-const e2 = document.querySelector('#bracket-header');
-const annotatetwo = RoughNotation.annotatetwo(e2, {type: 'crossthrough', color: 'black', padding: 7});
+const e3 = document.querySelector('#bracket-header');
+const annotatetwo = RoughNotation.annotatetwo(e3, {type: 'crossthrough', color: 'black', padding: 7});
 annotatetwo.show();
 
 
